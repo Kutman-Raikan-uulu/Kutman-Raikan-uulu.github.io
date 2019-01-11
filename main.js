03eb057ff06eaaec5061fdc1546802807f483c75
Kutman Raikan uulu <rajkanuulu@gmail.com>
2019-01-11 14:35:14 +0600

    added Js

--- main.js
+++ main.js
@@ -1,130 +1,75 @@
 function block11(){
-    
     document.getElementById('block1').style.display='block';
-    
 }
 function block21(){
-    
     document.getElementById('block2').style.display='none';
-    
 }
-function block31(){
-    
+function block31(){    
     document.getElementById('block3').style.display='none';
-    
 }
 function block41(){
-    
     document.getElementById('block4').style.display='none';
-    
 }
 function block51(){
-    
     document.getElementById('block5').style.display='none';
-    
 }
- 
-function block12(){
-    
+function block12(){   
     document.getElementById('block1').style.display='none';
-    
 }
 function block22(){
-    
     document.getElementById('block2').style.display='block';
-    
 }
 function block32(){
-    
     document.getElementById('block3').style.display='none';
-    
 }
 function block42(){
-    
     document.getElementById('block4').style.display='none';
-    
 }
 function block52(){
-    
     document.getElementById('block5').style.display='none';
-    
 }
- 
-function block13(){
-    
+function block13(){   
     document.getElementById('block1').style.display='none';
-    
 }
 function block23(){
-    
     document.getElementById('block2').style.display='none';
-    
 }
 function block33(){
-    
     document.getElementById('block3').style.display='block';
-    
 }
 function block43(){
-    
     document.getElementById('block4').style.display='none';
-    
 }
 function block53(){
-    
     document.getElementById('block5').style.display='none';
-    
 }
- 
-function block14(){
-    
+function block14(){   
     document.getElementById('block1').style.display='none';
-    
 }
 function block24(){
-    
     document.getElementById('block2').style.display='none';
-    
 }
 function block34(){
-    
     document.getElementById('block3').style.display='none';
-    
 }
 function block44(){
-    
     document.getElementById('block4').style.display='block';
-    
 }
 function block54(){
-    
     document.getElementById('block5').style.display='none';
-    
 }
- 
-function block15(){
-    
+function block15(){   
     document.getElementById('block1').style.display='none';
-    
 }
 function block25(){
-    
     document.getElementById('block2').style.display='none';
-    
 }
 function block35(){
-    
     document.getElementById('block3').style.display='none';
-    
 }
 function block45(){
-    
     document.getElementById('block4').style.display='none';
-    
 }
 function block55(){
-    
     document.getElementById('block5').style.display='block';
-    
 }
- 
