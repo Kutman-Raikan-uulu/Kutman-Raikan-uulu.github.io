488cf3af29104504b94bad25062d73bdd9b57b66
Kutman Raikan uulu <rajkanuulu@gmail.com>
2019-01-18 21:53:03 +0600

    new js

--- main.js
+++ main.js
@@ -1,75 +1,26 @@
-function block11(){
-    document.getElementById('block1').style.display='block';
-}
-function block21(){
-    document.getElementById('block2').style.display='none';
-}
-function block31(){    
-    document.getElementById('block3').style.display='none';
-}
-function block41(){
-    document.getElementById('block4').style.display='none';
-}
-function block51(){
-    document.getElementById('block5').style.display='none';
-}
-function block12(){   
-    document.getElementById('block1').style.display='none';
-}
-function block22(){
-    document.getElementById('block2').style.display='block';
-}
-function block32(){
-    document.getElementById('block3').style.display='none';
-}
-function block42(){
-    document.getElementById('block4').style.display='none';
-}
-function block52(){
-    document.getElementById('block5').style.display='none';
-}
-function block13(){   
-    document.getElementById('block1').style.display='none';
-}
-function block23(){
-    document.getElementById('block2').style.display='none';
-}
-function block33(){
-    document.getElementById('block3').style.display='block';
-}
-function block43(){
-    document.getElementById('block4').style.display='none';
-}
-function block53(){
-    document.getElementById('block5').style.display='none';
-}
-function block14(){   
-    document.getElementById('block1').style.display='none';
-}
-function block24(){
-    document.getElementById('block2').style.display='none';
-}
-function block34(){
-    document.getElementById('block3').style.display='none';
-}
-function block44(){
-    document.getElementById('block4').style.display='block';
-}
-function block54(){
-    document.getElementById('block5').style.display='none';
-}
-function block15(){   
-    document.getElementById('block1').style.display='none';
-}
-function block25(){
-    document.getElementById('block2').style.display='none';
-}
-function block35(){
-    document.getElementById('block3').style.display='none';
-}
-function block45(){
-    document.getElementById('block4').style.display='none';
-}
-function block55(){
-    document.getElementById('block5').style.display='block';
-}
+const navLinks = document.querySelectorAll('nav a');
+const articles = document.querySelectorAll('article');
+
+let activeLink = navLinks[0]; // Home
+
+navLinks.forEach(function(navLink) {
+  navLink.addEventListener('click', function(event) {
+    // Deactivate current page
+    activeLink.className = "";
+    const urlParts = activeLink.href.split('#');
+    const afterHashtag = urlParts.pop();
+    const oldArticle = document.getElementById(afterHashtag);
+    oldArticle.className = "";
+
+    // Set new page
+    activeLink = this;
+
+    // Activate new page
+    activeLink.className = "active";
+    document
+      .getElementById(activeLink.href.split('#').pop())
+      .className = "active";
+
+    event.preventDefault();
+  });
+});
