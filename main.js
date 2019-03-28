38edd201d2cea3b417f85b69725020ec8c2b280f
Kutman Raikan uulu <rajkanuulu@gmail.com>
2019-03-28 21:53:38 +0600

    Added js file

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const navLinks = document.querySelectorAll('nav a');
-const articles = document.querySelectorAll('article');
+const navLinks = document.querySelectorAll("nav a");
+const articles = document.querySelectorAll("content-article");
 
 let activeLink = navLinks[0]; // Home
 
 navLinks.forEach(function(navLink) {
-  navLink.addEventListener('click', function(event) {
+  navLink.addEventListener("click", function(event) {
     // Deactivate current page
-    activeLink.className = "";
-    const urlParts = activeLink.href.split('#');
+
+    const urlParts = activeLink.href.split("#");
     const afterHashtag = urlParts.pop();
     const oldArticle = document.getElementById(afterHashtag);
     oldArticle.className = "";
@@ -16,11 +16,23 @@ navLinks.forEach(function(navLink) {
     activeLink = this;
 
     // Activate new page
-    activeLink.className = "active";
-    document
-      .getElementById(activeLink.href.split('#').pop())
-      .className = "active";
+    activeLink.className = "content-active";
+    document.getElementById(activeLink.href.split("#").pop()).className =
+      "content-active";
 
     event.preventDefault();
   });
+});
+
+$(document).ready(function() {
+  $(".menu-icon").on("click", function() {
+    $("nav ul").toggleClass("showing");
+  });
+});
+$(window).on("scroll", function() {
+  if ($(window).scrollTop()) {
+    $("nav").addClass("black");
+  } else {
+    $("nav").removeClass;
+  }
 });
