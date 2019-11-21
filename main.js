084eb59ae161fbf319af1bb5b6788702fd381244
Kutman Raikan uulu <rajkanuulu@gmail.com>
2019-11-21 15:30:45 +0600

    star a new personality site

--- main.js
+++ main.js
@@ -96,3 +96,24 @@ textarea.addEventListener("input", function() {
 linkMailto.addEventListener("click", function() {
   linkMailto.href = "mailto:nurjigit.melis.uulu@gmail.com?subject=" + userText;
 });
+const anchors = document.querySelectorAll('a[href^="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute("href");
+
+    if (anchor.getAttribute("href") === "#") {
+      document.querySelector("body").scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+    } else {
+      document.querySelector("" + blockID).scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+    }
+  });
+}
