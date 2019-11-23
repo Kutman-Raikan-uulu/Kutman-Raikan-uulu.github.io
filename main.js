2bf594a0e4292f31f7d2a3ab93bbbbde6c1e454b
Kutman Raikan uulu <rajkanuulu@gmail.com>
2019-11-23 19:43:02 +0600

    added new parametres

--- main.js
+++ main.js
@@ -1,101 +1,3 @@
-const navLinks = document.querySelectorAll("nav a");
-const articles = document.querySelectorAll("content-article");
-
-let activeLink = navLinks[0]; // Home
-let textarea = document.querySelector("form textarea");
-let linkMailto = document.querySelector("form a");
-let inputFocus = document.querySelectorAll("input");
-let label = document.querySelectorAll("label");
-let email = document.querySelector(".email");
-let submitButton = document.querySelector("#submit");
-let downButton = document.querySelector("#down");
-let upButton = document.querySelector("#up");
-
-navLinks.forEach(function(navLink) {
-  navLink.addEventListener("click", function(event) {
-    // Deactivate current page
-
-    const urlParts = activeLink.href.split("#");
-    const afterHashtag = urlParts.pop();
-    const oldArticle = document.getElementById(afterHashtag);
-    oldArticle.className = "active";
-
-    activeLink.className = "";
-    // Set new page
-    activeLink = this;
-
-    // Activate new page
-    activeLink.className = "active";
-    document.getElementById(activeLink.href.split("#").pop()).className =
-      "content-active";
-
-    event.preventDefault();
-  });
-});
-
-$(document).ready(function() {
-  $(".menu-icon, nav ul").on("click", function() {
-    $("nav ul").toggleClass("showing");
-  });
-});
-$(window).on("scroll", function() {
-  if ($(window).scrollTop()) {
-    $("nav").addClass("black");
-  } else {
-    $("nav").removeClass;
-  }
-});
-$(".home-bg").on("click", "a", e =>
-  $("#it").attr("class", $(e.target).attr("id"))
-);
-
-email.addEventListener("change", function() {
-  email.className = "mail";
-});
-
-for (var i = 0; i < inputFocus.length; i++) {
-  let parentInput = inputFocus[i].parentElement;
-
-  inputFocus[i].addEventListener("input", function(event) {
-    if (this.value.length > 0) {
-      parentInput.classList.add("visible");
-      parentInput.classList.remove("hidden");
-    } else {
-      parentInput.classList.add("hidden");
-      parentInput.classList.remove("visible");
-    }
-
-    if (event.path[0].id == "name") {
-      username = this.value;
-    } else if (event.path[0].id == "surname") {
-      userSurname = this.value;
-    } else if (event.path[0].id == "address") {
-      userAddress = this.value;
-    } else if (event.path[0].id == "email") {
-      userEmail = this.value;
-    }
-  });
-
-  inputFocus[i].addEventListener("change", function() {
-    if (this.value.length > 0) {
-      parentInput.classList.add("good");
-      parentInput.classList.remove("hidden");
-      parentInput.classList.add("visible");
-    } else {
-      parentInput.classList.add("mistake");
-      parentInput.classList.remove("good");
-      parentInput.classList.add("visible");
-    }
-  });
-}
-
-textarea.addEventListener("input", function() {
-  userText = this.value;
-});
-
-linkMailto.addEventListener("click", function() {
-  linkMailto.href = "mailto:nurjigit.melis.uulu@gmail.com?subject=" + userText;
-});
 const anchors = document.querySelectorAll('a[href^="#"]');
 
 for (let anchor of anchors) {
@@ -117,3 +19,58 @@ for (let anchor of anchors) {
     }
   });
 }
+
+const navLinks = document.querySelectorAll("nav a");
+let activeLink = navLinks[0];
+
+navLinks.forEach(function(navLink) {
+  navLink.addEventListener("click", function(event) {
+    // Deactivate current page
+
+    navLink.className = "active";
+
+    activeLink.className = "";
+    // Set new page
+    activeLink = this;
+
+    // Activate new page
+    activeLink.className = "active";
+
+    event.preventDefault();
+  });
+});
+/* этот код помечает картинки, для удобства разработки */
+let i = 1;
+for (let li of carousel.querySelectorAll("li")) {
+  li.style.position = "relative";
+  li.insertAdjacentHTML(
+    "beforeend",
+    `<span style="position:absolute;left:0;top:0">${i}</span>`
+  );
+  i++;
+}
+
+/* конфигурация */
+let width = 500; // ширина картинки
+let count = 1; // видимое количество изображений
+
+let list = carousel.querySelector("ul");
+let listElems = carousel.querySelectorAll("li");
+
+let position = 0; // положение ленты прокрутки
+
+carousel.querySelector(".prev").onclick = function() {
+  // сдвиг влево
+  position += width * count;
+  // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+  position = Math.min(position, 0);
+  list.style.marginLeft = position + "px";
+};
+
+carousel.querySelector(".next").onclick = function() {
+  // сдвиг вправо
+  position -= width * count;
+  // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+  position = Math.max(position, -width * (listElems.length - count));
+  list.style.marginLeft = position + "px";
+};
